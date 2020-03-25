944ec35042596bc06d80552d4a9a21942578fc92
Dominique <>
2020-03-25 18:01:39 +0800

    Admin module: Add theme negotiation to implement custom login page with custom login theme.

---
+++ ek_admin/src/Theme/ThemeNegotiator.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\ek_admin\Theme\ThemeNegotiator
+ */
+namespace Drupal\ek_admin\Theme;
+
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Theme\ThemeNegotiatorInterface;
+
+class ThemeNegotiator implements ThemeNegotiatorInterface {
+
+    /**
+     * @param RouteMatchInterface $route_match
+     * @return bool
+     */
+    public function applies(RouteMatchInterface $route_match)
+    {
+        return $this->negotiateRoute($route_match) ? true : false;
+    }
+
+    /**
+     * @param RouteMatchInterface $route_match
+     * @return null|string
+     */
+    public function determineActiveTheme(RouteMatchInterface $route_match)
+    {
+        return $this->negotiateRoute($route_match) ?: null;
+    }
+
+    /**
+     * Function that does all of the work in selecting a theme
+     * @param RouteMatchInterface $route_match
+     * @return bool|string
+     */
+    private function negotiateRoute(RouteMatchInterface $route_match)
+    {
+
+        if ($route_match->getRouteName() == 'ek_admin.default'
+                && !\Drupal::currentUser()->isAuthenticated())
+        {
+            return 'ek_login';
+        } 
+        
+
+        return false;
+    }
+
+
+
+}
