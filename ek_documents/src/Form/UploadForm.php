c38f7be3e55dddaaded99f990be2d94bdb21624d
Dominique <>
2020-06-21 08:07:06 +0800

    Documents module: fixed warnings and coding standard.

--- ek_documents/src/Form/UploadForm.php
+++ ek_documents/src/Form/UploadForm.php
@@ -16,98 +16,91 @@ use Drupal\ek_documents\Settings;
 /**
  * Provides a form to upload document.
  */
-class UploadForm extends FormBase
-{
-
-  /**
-   * {@inheritdoc}
-   */
-    public function getFormId()
-    {
-        return 'ek_documents_upload';
-    }
-
+class UploadForm extends FormBase {
 
     /**
      * {@inheritdoc}
      */
-    public function buildForm(array $form, FormStateInterface $form_state)
-    {
+    public function getFormId() {
+        return 'ek_documents_upload';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(array $form, FormStateInterface $form_state) {
         $here = $this->getRouteMatch();
         if ($here->getRouteName() == 'ek_documents_documents_common') {
             $form['common'] = array(
-          '#type' => 'hidden',
-          '#value' => 1,
-          );
-        
+                '#type' => 'hidden',
+                '#value' => 1,
+            );
+
             $alert = "<div id='alert' class='messages messages--warning'>"
-                .t('File will be accessible in common area.'). "</div>";
+                    . t('File will be accessible in common area.') . "</div>";
             $form['alert'] = array(
-          '#type' => 'item',
-          '#markup' => $alert,
-          );
+                '#type' => 'item',
+                '#markup' => $alert,
+            );
         }
 
         $form['upload_doc'] = array(
-      '#type' => 'file',
-      '#title' => $this->t('Select file'),
-      );
+            '#type' => 'file',
+            '#title' => $this->t('Select file'),
+        );
 
         $form['folder'] = array(
-      '#type' => 'textfield',
-      '#size' => 20,
-      '#attributes' => array('placeholder' => $this->t('tag or folder') ),
-      '#autocomplete_route_name' => 'ek_look_up_folders',
-    );
-      
-    
+            '#type' => 'textfield',
+            '#size' => 20,
+            '#attributes' => array('placeholder' => $this->t('tag or folder')),
+            '#autocomplete_route_name' => 'ek_look_up_folders',
+        );
+
+
         $form['actions'] = array('#type' => 'actions');
         $form['actions']['upload'] = array(
             '#id' => 'upbuttonid1',
             '#type' => 'submit',
-            '#value' =>  $this->t('Upload') ,
+            '#value' => $this->t('Upload'),
             //'#attributes' => array('class' => array('use-ajax-submit')),
             '#ajax' => array(
-              'callback' => array($this, 'saveFile'),
-              'wrapper' => 'doc_upload_message',
-              'method' => 'replace',
-             ),
-      );
+                'callback' => array($this, 'saveFile'),
+                'wrapper' => 'doc_upload_message',
+                'method' => 'replace',
+            ),
+        );
 
-    
-    
+
+
         $form['doc_upload_message'] = array(
-      '#type' => 'item',
-      '#markup' => '',
-      '#prefix' => '<div id="doc_upload_message" class="red" >',
-      '#suffix' => '</div>',
-      
-      );
+            '#type' => 'item',
+            '#markup' => '',
+            '#prefix' => '<div id="doc_upload_message" class="red" >',
+            '#suffix' => '</div>',
+        );
 
- 
+
         return $form;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function validateForm(array &$form, FormStateInterface $form_state)
-    {
+    public function validateForm(array &$form, FormStateInterface $form_state) {
+        
     }
 
     /**
      * {@inheritdoc}
      */
-    public function submitForm(array &$form, FormStateInterface $form_state)
-    {
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+        
     }
-  
 
     /**
      * Save file callback
      */
-    public function saveFile(array &$form, FormStateInterface $form_state)
-    {
+    public function saveFile(array &$form, FormStateInterface $form_state) {
         if (!null == ($form_state->getValue('common'))) {
             $user = 0;
         } else {
@@ -115,14 +108,13 @@ class UploadForm extends FormBase
         }
         $settings = new Settings();
         //upload
-      
         //$extensions = 'csv png gif jpg jpeg bmp txt doc docx xls xlsx odt ods odp pdf ppt pptx sxc rar rtf tiff zip';
         $extensions = $settings->get('file_extensions');
-        $validators = array( 'file_validate_extensions' => array($extensions));
-        $dir = "private://documents/users/" . $user ;
+        $validators = array('file_validate_extensions' => array($extensions));
+        $dir = "private://documents/users/" . $user;
         \Drupal::service('file_system')->prepareDirectory($dir, FILE_CREATE_DIRECTORY | FILE_MODIFY_PERMISSIONS);
         $file = file_save_upload("upload_doc", $validators, $dir, 0, FILE_EXISTS_RENAME);
-         
+
         if ($file) {
             if ($settings->get('filter_char') == '1' && preg_match('/[^\00-\255]+/u', $file->getFileName())) {
                 //filter file name for special characters
@@ -130,32 +122,32 @@ class UploadForm extends FormBase
             } else {
                 $file->setPermanent();
                 $file->save();
-                $filename  = $file->getFileName();
+                $filename = $file->getFileName();
                 $uri = $file->getFileUri();
                 $fields = array(
-                      'uid' => $user,
-                      //'fid' => '',
-                      'type' => 0,
-                      'filename' => $filename,
-                      'uri' => $uri,
-                      'folder' => Xss::filter($form_state->getValue('folder')),
-                      'comment' => '',
-                      'date' => time(),
-                      'size' => filesize($uri),
-                      'share' => 0,
-                      'share_uid' => 0,
-                      'share_gid' => 0,
-                      'expire' => 0
-                    );
+                    'uid' => $user,
+                    //'fid' => '',
+                    'type' => 0,
+                    'filename' => $filename,
+                    'uri' => $uri,
+                    'folder' => Xss::filter($form_state->getValue('folder')),
+                    'comment' => '',
+                    'date' => time(),
+                    'size' => filesize($uri),
+                    'share' => 0,
+                    'share_uid' => 0,
+                    'share_gid' => 0,
+                    'expire' => 0
+                );
 
                 $insert = Database::getConnection('external_db', 'external_db')
-                      ->insert('ek_documents')
-                      ->fields($fields)
-                      ->execute();
+                        ->insert('ek_documents')
+                        ->fields($fields)
+                        ->execute();
 
-                $log = 'user ' . \Drupal::currentUser()->id() .'|'. \Drupal::currentUser()->getAccountName() .'|upload|'. $filename;
+                $log = 'user ' . \Drupal::currentUser()->id() . '|' . \Drupal::currentUser()->getAccountName() . '|upload|' . $filename;
                 \Drupal::logger('ek_documents')->notice($log);
-                $form['doc_upload_message']['#markup'] = $this->t('file uploaded @f', array('@f' => $filename ));
+                $form['doc_upload_message']['#markup'] = $this->t('file uploaded @f', array('@f' => $filename));
                 if ($user == 0) {
                     \Drupal\Core\Cache\Cache::invalidateTags(['common_documents']);
                 } else {
@@ -168,4 +160,5 @@ class UploadForm extends FormBase
 
         return $form['doc_upload_message'];
     }
+
 }
--- ek_documents/src/Settings.php
+++ ek_documents/src/Settings.php
@@ -11,50 +11,55 @@ namespace Drupal\ek_documents;
 use Drupal\Core\Database\Database;
 
 /**
- * Set and retrieve gloabal settings parameters
+ * Set and retrieve global settings parameters
  */
-  class Settings
-  {
-      public function __construct($uid = null)
-      {
-          $data = Database::getConnection('external_db', 'external_db')
-            ->query("SELECT id,settings FROM {ek_document_settings} WHERE id=:id", [':id' => 0])->fetchObject();
-    
-          $this->settings = unserialize($data->settings);
-      }
- 
-      /**
-       * Get a setting by setting name
-       */
-      public function get($name)
-      {
-          if (!empty($this->settings)) {
-              return  $this->settings[$name];
-          }
-      }
+class Settings {
 
-      /**
-       * Set a setting value by setting name
-       */
-      public function set($name, $value)
-      {
-          $this->settings[$name] = $value;
-      }
+    public function __construct($uid = null) {
+        $query = Database::getConnection('external_db', 'external_db')
+                    ->select('ek_document_settings', 'd')
+                    ->fields('d',['id','settings'])
+                    ->condition('id', 0)
+                    ->execute();
+        
+        if($data = $query->fetchObject()) {
+            $this->settings = unserialize($data->settings);
+        } else {
+            $this->settings = [];
+        }
+        
+    }
 
-      /**
-       * save settings
-       */
-      public function save()
-      {
-          $data = serialize($this->settings) ;
-          if (Database::getConnection('external_db', 'external_db')
-      ->update('ek_document_settings')
-      ->condition('id', 0)
-      ->fields(array(
-        'settings' => $data,
-      ))
-      ->execute()) {
-              return true;
-          }
-      }
-  }
+    /**
+     * Get a setting by setting name
+     */
+    public function get($name) {
+        if (!empty($this->settings)) {
+            return $this->settings[$name];
+        }
+    }
+
+    /**
+     * Set a setting value by setting name
+     */
+    public function set($name, $value) {
+        $this->settings[$name] = $value;
+    }
+
+    /**
+     * save settings
+     */
+    public function save() {
+        $data = serialize($this->settings);
+        if (Database::getConnection('external_db', 'external_db')
+                        ->update('ek_document_settings')
+                        ->condition('id', 0)
+                        ->fields(array(
+                            'settings' => $data,
+                        ))
+                        ->execute()) {
+            return true;
+        }
+    }
+
+}
