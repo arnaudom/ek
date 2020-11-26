cc3c1ecdef9e7fbc8e44e7e5a8134a8385f4c3be
Dominique <>
2020-11-26 10:49:36 +0800

    Admin: fixed error when installing new system and compatibility issue with Drupal 9 fix.

--- ek_admin/src/Plugin/Menu/AdminMenuLink.php
+++ ek_admin/src/Plugin/Menu/AdminMenuLink.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\ek_admin\Plugin\Menu;
 
+use Drupal\Core\Database\Database;
 use Drupal\Core\Menu\MenuLinkDefault;
 use Drupal\Core\Menu\StaticMenuLinkOverridesInterface;
 use Drupal\Core\Session\AccountInterface;
@@ -58,28 +59,29 @@ class AdminMenuLink extends MenuLinkDefault {
      * {@inheritdoc}
      */
     public function getTitle() {
-        
-            if ($this->currentUser->isAuthenticated() && $this->currentUser->hasPermission('administrate')) {
-                                 
-                    $coids = \Drupal\ek_admin\Access\AccessCheck::GetCompanyByUser();
-                    $api = \Drupal::moduleHandler()->invokeAll('ek_settings', [$coids]);
-                    $mod = ['documents','finance','logistics','projects','sales'];
-                    $c = 0;
-                    foreach($mod as $key) {
-                        if(array_key_exists($key, $api)) {
-                            $c++;
-                        }
+
+        if ($this->currentUser->isAuthenticated() && $this->currentUser->hasPermission('administrate')) {
+
+            $external = Database::getConnectionInfo('external_db');
+            if (!empty($external)) {
+                $coids = \Drupal\ek_admin\Access\AccessCheck::GetCompanyByUser();
+                $api = \Drupal::moduleHandler()->invokeAll('ek_settings', [$coids]);
+                $mod = ['documents', 'finance', 'logistics', 'projects', 'sales'];
+                $c = 0;
+                foreach ($mod as $key) {
+                    if (array_key_exists($key, $api)) {
+                        $c++;
                     }
-                    if ($c > 0) {
-                        return [
-                            '#markup' => $this->t('Administration <span title=@t class="admin_menu_badge">@c</span>', ['@t' => t('Settings missing'), '@c' => $c]),
-                            
-                        ];
-                    }
-                    return $this->t('Administration');
+                }
+                if ($c > 0) {
+                    return [
+                        '#markup' => $this->t('Administration <span title=@t class="admin_menu_badge">@c</span>', ['@t' => t('Settings missing'), '@c' => $c]),
+                    ];
+                }
             }
             return $this->t('Administration');
-        
+        }
+        return $this->t('Administration');
     }
 
     /**
