566eaf6342d8b22bb1616bbde4279aaa20d4d4bb
Dominique <>
2021-08-09 16:13:59 +0800

    Address book: fixed installation database error.

--- ek_address_book/src/Controller/InstallController.php
+++ ek_address_book/src/Controller/InstallController.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
-* @file
-* Contains \Drupal\ek\Controller\
-*/
+ * @file
+ * Contains \Drupal\ek\Controller\
+ */
 
 namespace Drupal\ek_address_book\Controller;
 
@@ -19,38 +20,39 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
-* Controller routines for ek module routes.
-*/
-class InstallController extends ControllerBase
-{
+ * Controller routines for ek module routes.
+ */
+class InstallController extends ControllerBase {
+    /* The module handler.
+     *
+     * @var \Drupal\Core\Extension\ModuleHandler
+     */
 
-   /* The module handler.
-   *
-   * @var \Drupal\Core\Extension\ModuleHandler
-   */
     protected $moduleHandler;
+
     /**
      * The database service.
      *
      * @var \Drupal\Core\Database\Connection
      */
     protected $database;
+
     /**
      * The form builder service.
      *
      * @var \Drupal\Core\Form\FormBuilderInterface
      */
     protected $formBuilder;
+
     /**
      * {@inheritdoc}
      */
-    public static function create(ContainerInterface $container)
-    {
+    public static function create(ContainerInterface $container) {
         return new static(
-      $container->get('database'),
-      $container->get('form_builder'),
-      $container->get('module_handler')
-    );
+                $container->get('database'),
+                $container->get('form_builder'),
+                $container->get('module_handler')
+        );
     }
 
     /**
@@ -61,21 +63,17 @@ class InstallController extends ControllerBase
      * @param \Drupal\Core\Form\FormBuilderInterface $form_builder
      *   The form builder service.
      */
-    public function __construct(Connection $database, FormBuilderInterface $form_builder, ModuleHandler $module_handler)
-    {
+    public function __construct(Connection $database, FormBuilderInterface $form_builder, ModuleHandler $module_handler) {
         $this->database = $database;
         $this->formBuilder = $form_builder;
         $this->moduleHandler = $module_handler;
     }
 
-
     /**
-       * install required tables in a separate database
-       *
-    */
-
-    public function install()
-    {
+     * install required tables in a separate database
+     *
+     */
+    public function install() {
         $query = "CREATE TABLE IF NOT EXISTS `ek_address_book` (
                 `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
                 `name` VARCHAR(250) NOT NULL DEFAULT '' COLLATE 'utf8mb4_unicode_ci',
@@ -85,7 +83,7 @@ class InstallController extends ControllerBase
                 `postcode` VARCHAR(50) NULL DEFAULT NULL,
                 `state` VARCHAR(50) NULL DEFAULT NULL COMMENT 'address state',
                 `city` VARCHAR(100) NULL DEFAULT NULL,
-                `country` VARCHAR(45) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',,
+                `country` VARCHAR(45) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
                 `telephone` VARCHAR(45) NULL DEFAULT NULL,
                 `fax` VARCHAR(45) NOT NULL DEFAULT '',
                 `website` VARCHAR(255) NULL DEFAULT NULL,
@@ -102,12 +100,12 @@ class InstallController extends ControllerBase
               COLLATE=utf8_general_ci
               ENGINE=InnoDB
               AUTO_INCREMENT=1";
-    
+
         $db = Database::getConnection('external_db', 'external_db')->query($query);
         if ($db) {
             $markup .= 'Address book table installed <br/>';
         }
-    
+
         $query = "CREATE TABLE IF NOT EXISTS `ek_address_book_contacts` (
                 `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
                 `abid` INT(10) UNSIGNED NOT NULL COMMENT 'id in address book',
@@ -130,7 +128,7 @@ class InstallController extends ControllerBase
               COLLATE=utf8_general_ci
             ENGINE=InnoDB
             AUTO_INCREMENT=1";
-            
+
         $db = Database::getConnection('external_db', 'external_db')->query($query);
         if ($db) {
             $markup .= 'Contacts table installed <br/>';
@@ -150,18 +148,21 @@ class InstallController extends ControllerBase
         if ($db) {
             $markup .= 'Comments table installed <br/>';
         }
-    
-    
+
+
         if (!$this->moduleHandler->moduleExists('ek_admin')) {
             $markup .= '<br/><b class="messages messages--warning">Main administration module is not installed. Please install this module in order to use Ek_logistics module.</b> <br/>';
         } else {
-            $link =  Url::fromRoute('ek_admin.main', array(), array())->toString();
+            $link = Url::fromRoute('ek_admin.main', array(), array())->toString();
             $markup .= '<br/>' . $this->t('You can proceed to further <a href="@c">settings</a>.', array('@c' => $link));
         }
-    
-        return  array(
-      '#title'=> $this->t('Installation of Ek_address_book module'),
-      '#markup' => $markup
-      ) ;
+
+        return array(
+            '#title' => $this->t('Installation of Ek_address_book module'),
+            '#markup' => $markup
+                );
     }
-} //class
+
+}
+
+//class
