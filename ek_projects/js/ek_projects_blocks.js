3ed0697e3b4838271b1948e87cfb5293b0dc8b46
Dominique <>
2023-08-01 15:50:07 +0800

    Projects: improve dashboard widgets with sortable and resizable options.

--- ek_projects/js/ek_projects_blocks.js
+++ ek_projects/js/ek_projects_blocks.js
@@ -21,6 +21,24 @@
                 }
             });
         });
+
+        jQuery(".dashcolumn").sortable({
+            connectWith: ".dashcolumn",
+            handle: ".widget-title",
+            cancel: ".widget-toggle",
+            placeholder: "widget widget-placeholder",
+            cursor: "move"
+        });
+
+        jQuery(".widget-toggle").on( "click", function() {
+            var icon = $(this);
+            icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+            icon.closest( ".widget" ).find( ".widget-content" ).toggle();
+        });
+        
+        jQuery(".widget-content" ).resizable({
+            handles: 's'
+        });
         
         
     });
