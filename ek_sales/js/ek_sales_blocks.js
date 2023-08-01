e98179841f8c3090c975aa2e4275d2a568ca24dc
Dominique <>
2023-08-01 16:49:10 +0800

    Sales: improve dashboard widgets with sortable and resizable options; fixed unknown warning for array key and deprecated code in default pdf documents.

---
+++ ek_sales/js/ek_sales_blocks.js
@@ -0,0 +1,29 @@
+(function ($, Drupal, drupalSettings) {
+
+    /* toggle the notify me value
+     * 
+     */
+    jQuery(function () {
+       
+        jQuery(".dashcolumn").sortable({
+            connectWith: ".dashcolumn",
+            handle: ".widget-title",
+            cancel: ".widget-toggle",
+            placeholder: "widget widget-placeholder",
+            cursor: "move"
+        });
+
+        jQuery(".widget-toggle").on( "click", function() {
+            var icon = $(this);
+            icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+            icon.closest( ".widget" ).find( ".widget-content" ).toggle();
+        });
+        
+        jQuery(".widget-content" ).resizable({
+            handles: 's'
+        });
+        
+        
+    });
+
+})(jQuery, Drupal, drupalSettings);
