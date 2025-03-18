3873ea8248db5d0f58d6a81d525635d95df9eac3
Dominique <>
2025-03-18 11:54:12 +0800

    Finance: fixed print page for single journal entry search.

--- ek_finance/js/ek_finance_journal.js
+++ ek_finance/js/ek_finance_journal.js
@@ -2,17 +2,42 @@
 
             Drupal.behaviors.journal = {
                 attach: function (context, settings) {
-                    jQuery('.print', context).on('click', function () {
-
-                        var printContents = document.getElementById("p" + this.id).innerHTML;
-                            $('<iframe>', {
-                                name: 'jiframe',
-                                class: 'printFrame'
-                            }).appendTo('body').contents().find('body').append(printContents);
-                            window.frames['jiframe'].focus();
-                            window.frames['jiframe'].print();
-                            setTimeout(function(){ $(".printFrame").remove(); }, 1000);
+                    
+                    $('.print', context).on('click', function () {
+                        var printContents = document.getElementById("p" + this.id).innerHTML;console.
+                        // Create an iframe
+                        var iframe = $('<iframe>', {
+                            name: 'chatframe',
+                            class: 'printFrame',
+                            style: 'display:none;' // Hide the iframe
+                        }).appendTo('body')[0];
+            
+                        iframe.onload = function() {
+                            // Get the iframe document
+                            var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+            
+                            // Set the content type and encoding
+                            iframeDoc.open();
+                            iframeDoc.write('<!DOCTYPE html>');
+                            iframeDoc.write('<html><head><title>Print</title>');
+                            iframeDoc.write('<meta charset="utf-8">');
+                            iframeDoc.write('</head><body>');
+                            iframeDoc.write(printContents);
+                            iframeDoc.write('</body></html>');
+                            iframeDoc.close();
+            
+                            // Focus and print the iframe content
+                            iframe.contentWindow.focus();
+                            iframe.contentWindow.print();
+            
+                            // Remove the iframe after printing
+                            setTimeout(function() {
+                                $(iframe).remove();
+                            }, 1000);
+                        };
+                        iframe.src = 'about:blank';
                     });
+            
                 }
             };
         })(jQuery, Drupal, drupalSettings);
